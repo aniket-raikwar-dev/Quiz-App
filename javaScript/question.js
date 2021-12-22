88a0f4823530f83f020e491fce5ad9593d4fab42
aniket-6234 <aniketashok1999@gmail.com>
2021-12-22 10:24:14 +0530

    question set

---
+++ javaScript/question.js
@@ -0,0 +1,64 @@
+//creating an array and passing the number, questions, options, and answer.
+let questions = [
+  {
+    numb: 1,
+    question: "Who is the developer of this app?",
+    answer: "Aniket A. Raikwar",
+    options: [
+      "Mark Zukerburg",
+      "Elon Musk",
+      "Aniket A. Raikwar",
+      "Jeff Bezos",
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "What does CSS Stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Cascading Style Sheet",
+      "Computer Software Security",
+      "Computer Stand Style",
+      "Common Software Settle",
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "Dat Structure - Stack follow which pattern?",
+    answer: "L I F O",
+    options: [
+      "L I F O",
+      "F I L O",
+      "F I F O",
+      "C I L O",
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "Select the Non-Primitive Data Types?",
+    answer: "Array",
+    options: [
+      "Boolean",
+      "String",
+      "Integer",
+      "Array",
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "Time Complexity of an Binary Search?",
+    answer: "O(log n)",
+    options: [
+      "O(N log n)",
+      "O(n)",
+      "O(log n)",
+      "O(2n)",
+    ]
+  },
+
+
+];
